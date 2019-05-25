14a47d4a2fdaaaef3ac1889e0f1b3b435da370e9
tzzLy7 <tianzizhao@outlook.com>
2019-05-25 10:36:09 +0800

    注册大致功能已实现，美化ing...

--- app/src/main/java/com/hebeu/meet/Register.java
+++ app/src/main/java/com/hebeu/meet/Register.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,8 @@ import com.hebeu.meet.domain.JSONResult;
 import com.hebeu.meet.domain.User;
 
 import java.util.Map;
+
+import javax.microedition.khronos.egl.EGLDisplay;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.http.HttpUtil;
@@ -27,15 +30,15 @@ import cn.hutool.json.JSONUtil;
 * 5-22
 * */
 public class Register extends AppCompatActivity {
-    private TextView user_id =null;
-    private TextView user_name =null;
+    private EditText user_id =null;
+    private EditText user_name =null;
     private Integer user_sex =null;
-    private TextView user_college =null;
-    private TextView user_classname =null;
-    private TextView user_qq =null;
-    private TextView user_phone =null;
-    private TextView user_email =null;
-    private TextView user_password=null;
+    private EditText user_college =null;
+    private EditText user_classname =null;
+    private EditText user_qq =null;
+    private EditText user_phone =null;
+    private EditText user_email =null;
+    private EditText user_password=null;
     private Button btn_register = null;
     private Handler handler = null;
 
@@ -45,9 +48,9 @@ public class Register extends AppCompatActivity {
         /*隐藏标题栏
          * */
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title bar  即隐藏标题栏
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notification bar  即全屏
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title bar  即隐藏标题栏
+//        getSupportActionBar().hide();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notification bar  即全屏
 
 
 
@@ -55,15 +58,17 @@ public class Register extends AppCompatActivity {
         handler = new Handler();
         System.out.println("register>>>>>");
         user_id=findViewById(R.id.user_id);
-        user_password=findViewById(R.id.user_password);
-        user_name=findViewById(R.id.user_name);
+        System.out.println(user_id+"00000000");
+        user_password=findViewById(R.id.user_password_mess);
+        user_name=findViewById(R.id.user_name_mess);
 //        user_sex=findViewById(R.id.user_sex);
-        user_college=findViewById(R.id.user_college);
-        user_classname=findViewById(R.id.user_classname);
-        user_qq=findViewById(R.id.user_qq);
-        user_phone=findViewById(R.id.user_phone);
-        user_email=findViewById(R.id.user_email);
+        user_college=findViewById(R.id.user_college_mess);
+        user_classname=findViewById(R.id.user_classname_mess);
+        user_qq=findViewById(R.id.user_qq_mess);
+        user_phone=findViewById(R.id.user_phone_mess);
+//        user_email=findViewById(R.id.user_email_mess);
         btn_register=findViewById(R.id.btn_register);
+        System.out.println("register.................................");
         Button imagemale=findViewById(R.id.imagemale);
         imagemale.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,7 +81,7 @@ public class Register extends AppCompatActivity {
         imagefalme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                user_sex=1;
+                user_sex=2;
                 System.out.println(user_sex);
             }
         });
@@ -91,18 +96,21 @@ public class Register extends AppCompatActivity {
                 @Override
                 public void run() {
                     User new_user = new User();
-                    System.out.println(123333333);
-                    System.out.println(String.valueOf(user_id.getText()));
-                    new_user.setUserId(String.valueOf(user_id.getText()));
-                    new_user.setPassword(String.valueOf(user_password.getText()));
-                    new_user.setUserName(String.valueOf(user_name.getText()));
+                    new_user.setUserId(String.valueOf(user_id.getEditableText()));
+                    System.out.println(user_id.getText());
+                    new_user.setPassword(String.valueOf(user_password.getEditableText()));
+                    System.out.println(user_password.getText());
+                    new_user.setUserName(String.valueOf(user_name.getEditableText()));
+                    System.out.println(user_name.getText());
                     new_user.setSex(user_sex);
+                    System.out.println(user_sex);
 //                    new_user.setSex(Integer.parseInt(String.valueOf(user_sex.getText())));
-                    new_user.setCollege(String.valueOf(user_college.getText()));
-                    new_user.setClassName(String.valueOf(user_classname.getText()));
-                    new_user.setQq(String.valueOf(user_qq.getText()));
-                    new_user.setPhone(String.valueOf(user_phone.getText()));
-                    new_user.setEmail(String.valueOf(user_email.getText()));
+                    new_user.setCollege(String.valueOf(user_college.getEditableText()));
+                    System.out.println(user_college.getText());
+                    new_user.setClassName(String.valueOf(user_classname.getEditableText()));
+                    new_user.setQq(String.valueOf(user_qq.getEditableText()));
+                    new_user.setPhone(String.valueOf(user_phone.getEditableText()));
+//                    new_user.setEmail(String.valueOf(user_email.getEditableText()));
                     Map<String,Object> paramMap = BeanUtil.beanToMap(new_user);
                     String res = "";
                     JSONResult jsonResult;
@@ -115,7 +123,7 @@ public class Register extends AppCompatActivity {
                         if (jsonResult.getCode() == 0){
                             //弹出对话框
                             Toast.makeText(Register.this,"注册成功！",Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(Register.this.getApplicationContext(), HomeActivity.class);
+                            Intent intent = new Intent(Register.this.getApplicationContext(), Login.class);
                             startActivity(intent);
 
                         }else {
@@ -126,7 +134,7 @@ public class Register extends AppCompatActivity {
 //                        Looper.prepare();
                         Toast.makeText(Register.this,"用户已存在",Toast.LENGTH_SHORT).show();
 //
-//                        Looper.loop();
+                        Looper.loop();
 
                     }
                         Looper.loop();
